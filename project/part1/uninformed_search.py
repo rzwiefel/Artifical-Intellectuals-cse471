068780d7ffd199d1abffcb9093a7e40f10588e62
mcmathews <mcmathe1@asu.edu>
2015-02-24 20:45:40 -0700

    Finished uninformed_search implementation.  Untested thus far

--- project/part1/uninformed_search.py
+++ project/part1/uninformed_search.py
@@ -1,37 +1,69 @@
 # Project, Part 1, Uninformed Search
 
 from networkx import nx
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from queue import PriorityQueue
+import time
+
+
+MAX_CARDS = 10
 
 
 def load_data():
     G = nx.Graph()
     with open("348.edges.txt", "r") as f:
         for line in f.readlines():
-            G.add_edge(tuple(map(int, line.split())))
+            G.add_edge(*tuple(map(int, line.split())))
 
     return G
 
-def path_cost(G, n):
-    pass
+
+def exposed_graph(G, node):
+    H = nx.Graph()
+    for i in node:
+        H.add_edges_from([(i, n) for n in G.neighbors(i)])
+
+    return H
+
+
+def path_cost(G, node):
+    population = G.number_of_nodes()
+
+    cost = 0
+    exposed_people = nx.Graph()
+    for i, person in enumerate(node):
+        n_exposed = exposed_people.number_of_nodes()
+        n_given_card = i
+
+        new_exposed_people = G.neighbors(person)
+        new_exposed = len(new_exposed_people)
+
+        adoption_probability = max(.1, 1 - 1 / (n_exposed + n_given_card)) if n_exposed + n_given_card != 0 else .1
+        cost += population - 1 - new_exposed * adoption_probability
+
+        exposed_people.add_nodes_from(new_exposed_people)
+
+    return cost
+
+
+def card_candidates(G, node):
+    H = exposed_graph(G, node)
+    I = G.copy()
+    I.remove_nodes_from(H)
+    return I.nodes()
 
 
 def goal_test(G, node):
-    if len(node) == 10:
+    if len(node) == MAX_CARDS:
         return True
-    H = nx.Graph()
-    for i in node:
-        H.add_node(G.neighbors(i))
-        H.add_node(i)
-    I = nx.symmetric_difference(G, H)
-    return I.nodes().empty()
+
+    return len(card_candidates(G, node)) == 0
+
 
 def uniform_cost_search(G):
     frontier = PriorityQueue()
-    explored = []
-    node = []
-    frontier.put((0, node))
+
+    frontier.put((0, []))
     while "pigs" != "fly":
         if frontier.empty():
             raise Exception("Not solution found")
@@ -40,14 +72,20 @@ def uniform_cost_search(G):
         if goal_test(G, node):
             return node
 
-        
+        for candidate in card_candidates(G, node):
+            child = node[:] + [candidate]
+            frontier.put((path_cost(G, child), child))
 
 
 def main():
     print("running project.part1.uninformed_search")
 
+    start = time.time()
+
     G = load_data()
-    uniform_cost_search(G)
+    solution = uniform_cost_search(G)
+    print(solution)
+    print("Time: " + str(time.time() - start))
 
 if __name__ == '__main__':
     main()
